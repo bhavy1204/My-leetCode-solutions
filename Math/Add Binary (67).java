eb06aaffe92fe43de5ee2a1cc5155417a690ce22
bhavy1204 <bhavysarwa51@gmail.com>
2025-07-16 08:27:49 +0530

    solve: Add binary(67) (easy)

--- Math/Add Binary (67).java
+++ Math/Add Binary (67).java
@@ -1,7 +1,5 @@
 // Given two binary strings a and b, return their sum as a binary string.
 
- 
-
 // Example 1:
 
 // Input: a = "11", b = "1"
@@ -19,3 +17,22 @@
 // Each string does not contain leading zeros except for the zero itself.
 
 // MY SOLUTION
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sum = new StringBuilder();
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry =0;
+
+        while(i>=0 || j>=0 || carry!=0){
+            int A = (i>=0)? a.charAt(i--) - '0' : 0;
+            int B = (j>=0)? b.charAt(j--) - '0' : 0;
+
+            int sum2 = A+B+carry;
+            sum.append(sum2%2);
+            carry = sum2/2;
+        }
+
+        return sum.reverse().toString();
+    }
+}
